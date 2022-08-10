af389717dfd894a62f3543057b1c26549bf4bfa8
MADHUSRIJ <srimadhu.j@gmail.com>
2022-08-10 20:04:40 +0530

    10 08 2022 08 00

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-employee-list',
@@ -10,23 +11,16 @@ export class EmployeeListComponent implements OnInit {
 
   employees!: Employee[];
 
-  constructor() { }
+  constructor(private employeeService: EmployeeService) { }
 
   ngOnInit(): void {
-    this.employees = [
-      {
-        "id": 1,
-        "firstName": "Madhu",
-        "lastName": "J",
-        "emailId": "srimadhu.j@gmail.com"
-    },
-    {
-        "id": 2,
-        "firstName": "Nithin",
-        "lastName": "J",
-        "emailId": "nithinraaj@gmail.com"
-    }
-    ];
+    this.getEmployees();
+  }
+
+  private getEmployees(){
+    this.employeeService.getEmployeesList().subscribe(data => {
+      this.employees = data;
+    });
   }
 
 }
